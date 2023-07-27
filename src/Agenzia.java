1fb1cebe4d63d2d24425fb015aa09f65b0303799
Burdusa Daniel <danielburdusa26@gmail.com>
2023-07-27 17:58:49 +0200

    finito

--- src/Agenzia.java
+++ src/Agenzia.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class Agenzia {
-}
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Agenzia {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean exit = true;
+        while (exit) {
+            try {
+            System.out.println("Vuoi inserire una vacanza (premi 1) o vuoi uscire (premi 0) ? ");
+            int scelta = input.nextInt();
+            if (scelta == 0) {
+                exit = false;
+            } else if (scelta == 1) {
+                System.out.println("Inserire la destinazione:");
+                input.nextLine();
+                String destinazione = input.nextLine();
+
+                System.out.println("Inserire la data di inizio della vacanza (ES: yyyy-MM-dd):");
+                LocalDate dataInizio = LocalDate.parse(input.nextLine());
+
+                System.out.println("Inserire la data della fine della vacanza (ES: yyyy-MM-dd):");
+                LocalDate dataFine = LocalDate.parse(input.nextLine());
+
+                Vacanza vacanza = new Vacanza(destinazione, dataInizio, dataFine);
+                System.out.println("Hai prenotato una vacanza di " + vacanza.durataVacanza() + " giorni a " +
+                        vacanza.getDestinazione() + " dal " + vacanza.getDataInizio() + " al " + vacanza.getDataFine());
+            } else {
+                System.out.println("Comando non valido. Riprova.");
+            }
+            } catch (RuntimeException e) {
+                System.out.println("Errore durante la prenotazione: " + e.getMessage());
+            }
+        }
+
+
+        }
+    }
+
--- src/Vacanza.java
+++ src/Vacanza.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Vacanza {
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Vacanza {
+private String destinazione;
+private LocalDate dataInizio;
+private LocalDate dataFine;
+
+    public Vacanza(String destinazione, LocalDate dataInizio, LocalDate dataFine) throws RuntimeException{
+        if (destinazione == null|| destinazione.isEmpty()){
+            throw new RuntimeException("the destination must not be null");
+        }
+        if (dataInizio == null||dataInizio.isBefore(LocalDate.now())){
+            throw new RuntimeException("the date for the start of the vacantion is invalid");
+        }
+        if(dataFine == null||dataFine.isBefore(dataInizio)){
+            throw new RuntimeException("the date for the end of the vacantion is invalid");
+        }
+        this.destinazione = destinazione;
+        this.dataInizio = dataInizio;
+        this.dataFine = dataFine;
+    }
+    public String getDestinazione() {
+        return destinazione;
+    }
+    public LocalDate getDataInizio() {
+        return dataInizio;
+    }
+    public LocalDate getDataFine() {
+        return dataFine;
+    }
+    public int durataVacanza(){
+        Period vacantion = Period.between(dataInizio,dataFine);
+        return vacantion.getDays();
+    }
 }
